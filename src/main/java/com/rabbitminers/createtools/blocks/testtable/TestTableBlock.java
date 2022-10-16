5c5c4e650f5a90c9b9d4ef6e2681d98ff982220a
Charlie Wood <rabbitminers2.0@gmail.com>
2022-10-16 17:43:51 +0100

    Implemented Entity For Drafting Table

---
+++ src/main/java/com/rabbitminers/createtools/blocks/testtable/TestTableBlock.java
@@ -0,0 +1,70 @@
+package com.rabbitminers.createtools.blocks.testtable;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.stream.Stream;
+
+public class TestTableBlock extends Block implements EntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public TestTableBlock(Properties properties) {
+        super(properties);
+    }
+
+    public InteractionResult use(BlockState p_54281_, Level level, BlockPos blockPos, Player p_54284_, InteractionHand p_54285_, BlockHitResult p_54286_) {
+        System.out.println(p_54281_);
+        return InteractionResult.CONSUME;
+    }
+
+    private static final VoxelShape SHAPE = Stream.of(Block.box(0, 0, 0, 16, 16, 16)).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.rotate(pMirror.getRotation(pState.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+    public BlockEntity newBlockEntity(BlockPos p_153451_, BlockState p_153452_) {
+        return new TestTableBlockEntity(p_153451_, p_153452_);
+    }
+}
---
+++ src/main/java/com/rabbitminers/createtools/blocks/testtable/TestTableBlockEntity.java
@@ -0,0 +1,57 @@
+package com.rabbitminers.createtools.blocks.testtable;
+
+import com.rabbitminers.createtools.index.CTBlockEntities;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ButtonBlock;
+import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+
+public class TestTableBlockEntity extends BlockEntity {
+
+
+    public TestTableBlockEntity(BlockPos p_155229_, BlockState p_155230_) {
+        super(CTBlockEntities.TEST_TABLE.get(), p_155229_, p_155230_);
+    }
+
+    public InteractionResult use(
+            BlockState state,
+            Level level,
+            BlockPos pos,
+            Player player,
+            InteractionHand hand,
+            BlockHitResult blockHitResult
+    ) {
+        CompoundTag nbt;
+        ItemStack heldItem = player.getItemInHand(hand);
+
+        System.out.println("AA");
+
+        if (heldItem.getItem() == Items.AIR || heldItem.getCount() != 1)
+            return InteractionResult.sidedSuccess(level.isClientSide());
+
+
+        return InteractionResult.sidedSuccess(level.isClientSide);
+    }
+
+}
