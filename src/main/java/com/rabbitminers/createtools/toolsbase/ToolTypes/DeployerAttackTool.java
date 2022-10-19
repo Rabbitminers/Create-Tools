5a643f3d1d33f51f8ab18235576fb42d5de9b2d8
Charlie Wood <rabbitminers2.0@gmail.com>
2022-10-19 19:22:48 +0100

    Implemented Attack Mode For Deployer

---
+++ src/main/java/com/rabbitminers/createtools/toolsbase/ToolTypes/DeployerAttackTool.java
@@ -0,0 +1,10 @@
+package com.rabbitminers.createtools.toolsbase.ToolTypes;
+
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+
+public class DeployerAttackTool extends SwordItem {
+    public DeployerAttackTool(Tier tier, int i, float v, Properties properties) {
+        super(tier, i, v, properties);
+    }
+}
