8083f449b1d94913f939b25b61183dc29f4c4254
Charlie Wood <rabbitminers2.0@gmail.com>
2022-11-14 17:21:45 +0000

    Added new base tool types

---
+++ src/main/java/com/rabbitminers/createtools/client/DraftingTableHudOverlay.java
@@ -0,0 +1,15 @@
+package com.rabbitminers.createtools.client;
+
+import com.rabbitminers.createtools.CreateTools;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class DraftingTableHudOverlay {
+    @SubscribeEvent
+    public void onRenderGui(RenderGameOverlayEvent.Post event) {
+
+    }
+}
